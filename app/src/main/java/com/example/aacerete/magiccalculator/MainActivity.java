a688d93bc6c97e204508499b2cdc09091389a855
aacerete <46990527d@iespoblenou.org>
2017-06-17 16:18:23 +0200

    3 modes

--- app/src/main/java/com/example/aacerete/magiccalculator/MainActivity.java
+++ app/src/main/java/com/example/aacerete/magiccalculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.aacerete.magiccalculator;
+import android.content.SharedPreferences;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.InputType;
 import android.view.MotionEvent;
 import android.widget.Button;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -11,7 +15,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.faendir.rhino_android.RhinoAndroidHelper;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ImporterTopLevel;
+import org.mozilla.javascript.Scriptable;
 
 import me.grantland.widget.AutofitHelper;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -21,19 +34,44 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView display;
+    TextView markbtnC;
+    TextView markbtnPercentage;
+    TextView markbtnOperator;
 
-    String modeAction = "";
+    String modeAction = null;
+    long modeCronoStart = 0;
+    long modeCronoStop = 0;
+    String modeAdivina = null;
+
+    private Context context;
+    private Scriptable scope;
+    private RhinoAndroidHelper rhinoAndroidHelper;
+    private boolean modeActionActived;
+    private SharedPreferences settings;
+    private EditText vibracionCorta;
+    private EditText vibracionLarga;
+    private EditText intervaloVibracion;
+    private EditText intervaloDigito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initFontStyle();
         setContentView(R.layout.activity_main);
-
+        settings = getSharedPreferences("Magic", MODE_PRIVATE);
         display = (TextView) findViewById(R.id.display);
         AutofitHelper.create(display);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        markbtnC = (TextView) findViewById(R.id.markbtnC);
+        markbtnC.setVisibility(View.INVISIBLE);
+
+        markbtnPercentage = (TextView) findViewById(R.id.markbtnPercentage);
+        markbtnPercentage.setVisibility(View.INVISIBLE);
+
+        markbtnOperator = (TextView) findViewById(R.id.markbtnOperator);
+        markbtnOperator.setVisibility(View.INVISIBLE);
 
         initButtons();
 
@@ -223,6 +261,26 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        display.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        if(modeAction != null && modeAction.equals("%")){
+                            if(modeCronoStart == 0){
+                                modeCronoStart = event.getEventTime();
+                            }else{
+                                display.setText(""+(event.getEventTime()-modeCronoStart));
+                            }
+                        }
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     private void handleButtonMode(Button v, long eventTime, long downTime) {
@@ -234,7 +292,11 @@ public class MainActivity extends AppCompatActivity
             // do simple action
             switch(v.getText().toString()){
                 case "C":
-                    display.setText("");
+                    if(modeAction != null && modeAction.equals("+/-")){
+                        modeAdivina = display.getText().toString();
+                        System.out.println("GUARDAMOS ADIVINANZA: "+modeAdivina);
+                    }
+                    display.setText("0");
                     break;
 
                 case "+/-":
@@ -254,16 +316,90 @@ public class MainActivity extends AppCompatActivity
     private void handleButtonClick(Button btn){
         String code = btn.getText().toString();
 
-        if(code.equals("=") && !modeAction.isEmpty()){
+        if(code.equals("=") && modeAction != null && !modeActionActived){
             System.out.println("HABILITAMOS MODO -> "+modeAction);
             if(modeAction.equals("C")){
-
+                modeActionActived = true;
+                markbtnC.setVisibility(View.VISIBLE);
             }
-            modeAction = null;
+            if(modeAction.equals("%")){
+                modeActionActived = true;
+                markbtnPercentage.setVisibility(View.VISIBLE);
+            }
+            if(modeAction.equals("+/-")){
+                modeActionActived = true;
+                markbtnOperator.setVisibility(View.VISIBLE);
+            }
+            display.setText("0");
+            //modeAction = null;
             return;
         }
 
-        if(display.getText().equals("0")){
+        if(code.contains("=")){
+            if(modeActionActived){
+                if(modeAction.equals("C")) {
+                    display.setText(settings.getString("num_guardado", "6666"));
+                    markbtnC.setVisibility(View.INVISIBLE);
+                }else if(modeAction.equals("+/-")){
+                    markbtnOperator.setVisibility(View.INVISIBLE);
+                    Vibrator v = (Vibrator) getSystemService(android.content.Context.VIBRATOR_SERVICE);
+                    if(modeAdivina != null) {
+                        for (int i = 0; i < modeAdivina.length(); i++) {
+                            char charact = modeAdivina.charAt(i);
+                            if (charact != '.') {
+                                Integer num = Integer.parseInt("" + charact);
+                                try {
+                                    if (num >= 5) {
+                                        while (num > 0) {
+                                            if (num >= 5) {
+                                                v.vibrate(Long.parseLong(settings.getString("vibracion_larga", "500")));
+                                                num = num - 5;
+                                            } else {
+                                                v.vibrate(Long.parseLong(settings.getString("vibracion_corta", "100")));
+                                                num = num - 1;
+                                            }
+                                            Thread.sleep(Long.parseLong(settings.getString("intervalo_vibracion", "500")));
+                                        }
+                                    } else if (num == 0) {
+                                        v.vibrate(Long.parseLong(settings.getString("vibracion_corta", "100")));
+                                        Thread.sleep(Long.parseLong(settings.getString("intervalo_vibracion", "500")));
+                                        v.vibrate(Long.parseLong(settings.getString("vibracion_corta", "100")));
+                                    } else {
+                                        for (int j = 0; j < num; j++) {
+                                            v.vibrate(Long.parseLong(settings.getString("vibracion_corta", "100")));
+                                            Thread.sleep(Long.parseLong(settings.getString("intervalo_vibracion", "500")));
+                                        }
+                                    }
+                                    Thread.sleep(Long.parseLong(settings.getString("intervalo_digito", "2000")));
+                                } catch (InterruptedException e) {
+                                    e.getLocalizedMessage();
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    display.setText("0");
+                }
+                modeAction = null;
+                modeActionActived = false;
+            }else {
+                rhinoAndroidHelper = new RhinoAndroidHelper(this);
+                context = rhinoAndroidHelper.enterContext();
+                context.setOptimizationLevel(-1);
+                scope = new ImporterTopLevel(context);
+                try {
+                    Object result = context.evaluateString(scope, display.getText().toString().replaceAll("X", "*"), "JavaScript", 1, null);
+                    display.setText(Context.toString(result));
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                } finally {
+                    context.exit();
+                }
+            }
+            return ;
+        }
+
+        if(display.getText().toString().equals("0")){
             display.setText(code);
         }else {
             display.append(code);
@@ -271,7 +407,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void attachBaseContext(Context newBase) {
+    protected void attachBaseContext(android.content.Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
@@ -321,9 +457,42 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_num) {
+            new MaterialDialog.Builder(this)
+                    .title("Número guardado")
+                    .content("Introduce el número a forzar")
+                    .inputType(InputType.TYPE_CLASS_TEXT)
+                    .input("0", settings.getString("num_guardado", "6666"), new MaterialDialog.InputCallback() {
+                        @Override
+                        public void onInput(MaterialDialog dialog, CharSequence input) {
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putString("num_guardado", input.toString());
+                            editor.commit();
+                        }
+                    }).show();
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_adivina) {
+            MaterialDialog dialog = new MaterialDialog.Builder(this)
+                    .title("Configuración Adivinanza")
+                    .customView(R.layout.adivinanza_custom, true)
+                    .positiveText("Guardar")
+                    .onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            guardarAdivanzaSettings(vibracionCorta.getText().toString(), vibracionLarga.getText().toString(), intervaloVibracion.getText().toString(), intervaloDigito.getText().toString());
+                        }
+                    })
+                    .build();
+            vibracionCorta = (EditText) dialog.getCustomView().findViewById(R.id.vibracion_corta);
+            vibracionCorta.setText(settings.getString("vibracion_corta", "100"));
+            vibracionLarga = (EditText) dialog.getCustomView().findViewById(R.id.vibracion_larga);
+            vibracionLarga.setText(settings.getString("vibracion_larga", "500"));
+            intervaloVibracion = (EditText) dialog.getCustomView().findViewById(R.id.intervalo_vibracion);
+            intervaloVibracion.setText(settings.getString("intervalo_vibracion", "500"));
+            intervaloDigito = (EditText) dialog.getCustomView().findViewById(R.id.intervalo_digito);
+            intervaloDigito.setText(settings.getString("intervalo_digito", "2000"));
+
+            dialog.show();
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -339,4 +508,14 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void guardarAdivanzaSettings(String corta, String larga, String vibracion, String digito){
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("vibracion_corta", corta);
+        editor.putString("vibracion_larga", larga);
+        editor.putString("intervalo_vibracion", vibracion);
+        editor.putString("intervalo_digito", digito);
+        editor.commit();
+    }
+
 }
